337ae3560b4ffdd234eb44745fed13b656761167
chenhuimin <chm141593@alibaba-inc.com>
2017-10-19 17:14:14 +0800

    add bat file

---
+++ start-proxy.bat
@@ -0,0 +1,7 @@
+@echo off
+rem SET WEBFLODER=d:\www\nodesite
+
+rem chdir /d %WEBFLODER%
+set /p password=Please input password: 
+echo forever starting...
+forever -p . -l ./logs/access.log -e ./logs/error.log start --uid "proxy" local.js -s my-shadow1.herokuapp.com -l 1080 -m aes-256-cfb -k %password% -p 80
