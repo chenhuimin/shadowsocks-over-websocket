d49c21e4fd9791f54a4ae1ab059cbb1b1fdc2500
chenhuimin <349196770@qq.com>
2018-06-17 00:06:49 +0800

    update

--- start-proxy.bat
+++ start-proxy.bat
@@ -4,5 +4,5 @@ rem SET WEBFLODER=d:\www\nodesite
 rem chdir /d %WEBFLODER%
 rem set /p password=Please input password: 
 echo forever starting...
-call forever -p . -a -l ./logs/access.log -e ./logs/error.log start --uid "proxy" local.js -s my-shadow1.herokuapp.com -l 1080 -m aes-256-cfb -k Chenhuimin1986 -p 80 --log-level error
+call forever -p . -a -l ./logs/access.log -e ./logs/error.log start --uid "proxy" local.js -s my-shadow1.herokuapp.com -l 1080 -m aes-256-cfb -k chenhuimin1986 -p 80 --log-level error
 pause
